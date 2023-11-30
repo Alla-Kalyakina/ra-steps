6b482c3b051f714881adf4731fcc0636a601b0f3
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-11-30 15:54:14 +0300

    edit4

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { HTMLProps, useState } from 'react'
+import Form from './components/Form'
 import './App.css'
-import AddingData from './components/AddingData'
-
-export interface Form {
-  date: string
-  distance: number
-}
 
 function App() {
 
-  const [form, setForm] = useState<Form>({
-    date:'',
-    distance: 0
-  })
-
-  const [arrForm, setArrForm] = useState<Form[]>([])
-
-  const handleFormChange = ({target}: React.FormEvent<HTMLInputElement>) => {
-    const {name, value} = target as HTMLInputElement;
-    setForm(preform => ({...preform, [name]: value}))
-  }
-  
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setArrForm(preform => [...preform, form])
-  }
-
-  function handleClickRemove(date: string, distance: number) {
-    setArrForm(arrForm.filter((elem)=> (elem.date != date && elem.distance != distance)));    
-  }
-
-  arrForm.sort((a, b) => {
-    if (a.date < b.date) {
-      return -1;
-    }
-    if (a.date > b.date) {
-      return 1;
-    }
-    return 0;
-  })
-
-  const acc = arrForm.reduce((acc, entry) => {
-    const date = entry.date
-    const repeatElem: Form = acc.find(elem => elem.date === date)
-    if(repeatElem !== undefined){
-      repeatElem.distance += entry.distance
-    } else {
-        acc.push(entry)
-    }
-    console.log(acc)
-    return acc
-  }, [])
-
   return (
     <>
-      <form action="/" method="post" onSubmit={handleFormSubmit}>
-          <div className="form-control">
-            <label htmlFor="date" className="label">
-              <span className="label-text">Дата (ДД.ММ.ГГГГ)</span>
-            </label>
-            <input  
-                id= "date" name="date"
-                value={form.date}
-                onChange={handleFormChange}/>
-          </div>
-          <div className="form-control">
-            <label htmlFor="distance" className="label">
-              <span className="label-text">Пройдено, км</span>
-            </label>
-            <input  
-                id= "distance" name="distance"
-                value={form.distance}
-                onChange={handleFormChange}/>
-          </div>
-          <button className="btn" type="submit">OK</button>
-      </form>
-      <AddingData arrForm={acc}
-      handleClickRemove={handleClickRemove}/>
+      <Form/>
     </>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react'
+import AddingData from './AddingData'
+
+export interface Form {
+  date: string
+  distance: number
+}
+
+export default function Form() {
+
+  const [form, setForm] = useState<Form>({
+    date:'',
+    distance: 0
+  })
+
+  const [arrForm, setArrForm] = useState<Form[]>([])
+
+  const handleDataChange = ({target}: React.FormEvent<HTMLInputElement>) => {
+    const {name, value} = target as HTMLInputElement;
+    setForm(preform => ({...preform, [name]: value}))
+  }
+
+  const handleDistanceChange = ({target}: React.FormEvent<HTMLInputElement>) => {
+    const {name, value} = target as HTMLInputElement;
+    setForm(preform => ({...preform, [name]: Number(value)}))
+  }
+  
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+    event.preventDefault();
+    setArrForm(preform => [...preform, form].reduce((acc, entry) => {
+      const date = entry.date
+      const repeatElem: Form = acc.find(elem => elem.date === date)
+      if(repeatElem !== undefined){
+        repeatElem.distance += entry.distance
+      } else {
+          acc.push(entry)
+      }
+      return acc
+    }, []))
+  }
+
+  function handleClickRemove(date: string, distance: number) {
+    setArrForm(arrForm.filter((elem)=> (elem.date != date && elem.distance != distance)));    
+  }
+
+  arrForm.sort((a, b) => {
+    if (a.date < b.date) {
+      return -1;
+    }
+    if (a.date > b.date) {
+      return 1;
+    }
+    return 0;
+  })
+
+  return (
+    <>
+      <form action="/" method="post" onSubmit={handleFormSubmit}>
+          <div className="form-control">
+            <label htmlFor="date" className="label">
+              <span className="label-text">Дата (ДД.ММ.ГГГГ)</span>
+            </label>
+            <input  
+                id= "date" name="date"
+                value={form.date}
+                onChange={handleDataChange}/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="distance" className="label">
+              <span className="label-text">Пройдено, км</span>
+            </label>
+            <input  
+                id= "distance" name="distance"
+                value={form.distance}
+                onChange={handleDistanceChange}/>
+          </div>
+          <button className="btn" type="submit">OK</button>
+      </form>
+      <AddingData arrForm={arrForm}
+      handleClickRemove={handleClickRemove}/>
+    </>
+  )
+}
